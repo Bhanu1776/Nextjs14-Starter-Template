b0b83807bf3fbf377a64aef26dc4163b0874d567
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-06-22 01:13:15 +0530

    style: Added twitter card
    Beautify links with cards while sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,16 @@ export default function RootLayout({
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png" />
         <meta name="theme-color" content="#e0c8fd" />
+
+        <meta property="og:title" content="Nextjs App" />
+        <meta name="twitter:title" content="Nextjs App" />
+
+        <meta name="description" content="Nextjs 13 starter template" />
+        <meta property="og:description" content="Nextjs 13 starter template" />
+        <meta name="twitter:description" content="Nextjs 13 starter template" />
+
+        <meta property="og:image" content="https://i.imgur.com/Z3bMJXy.jpg" />
+        <meta name="twitter:image" content="https://i.imgur.com/Z3bMJXy.jpg" />
       </head>
       <body
         className={classNames({
